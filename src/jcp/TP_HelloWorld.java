2328401a0733690e18db3adafdd387bfe49e09fc
Shawn Brito <shawnbrito@gmail.com>
2021-06-24 13:19:30 +0530

    Added a helloworld and Jenkins file, Thats all.

---
+++ src/jcp/TP_HelloWorld.java
@@ -0,0 +1,15 @@
+package jcp;
+
+public class TP_HelloWorld {
+	
+	public TP_HelloWorld()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TP_HelloWorld();
+	}
+
+}
